e6de0bb6a049abaae4b5b28226e99f9076e3678b
vadim <vadim@Vadzims-MBP.fios-router.home>
2017-12-04 07:46:17 -0500

    update settings

--- seeking_alpha/settings.py
+++ seeking_alpha/settings.py
@@ -19,25 +19,25 @@ RETRY_TIMES = 10  # initial response + 2 retries = 3 requests
 RETRY_HTTP_CODES = [500, 502, 503, 504, 408]
 RETRY_PRIORITY_ADJUST = -1
 
+CONCURRENT_REQUESTS = 1
 DOWNLOADER_STATS = False
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
 
 
 # # Crawl responsibly by identifying yourself (and your website) on the user-agent
 
 DOWNLOADER_MIDDLEWARES = {
     'seeking_alpha.middlewares.RotateUserAgentMiddleware': 110,
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': 150,
+    'scrapy.downloadermiddlewares.cookies.CookiesMiddleware': 200
 }
 
 USER_AGENT_CHOICES = [
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
-    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)',
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36',
-    'Mozilla/5.0 (X11; Linux x86_64; rv:24.0) Gecko/20140205 Firefox/24.0 Iceweasel/24.3.0',
-    'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0',
-    'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:28.0) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)"
 ]
 
 EXTENSIONS = {
